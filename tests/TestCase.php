2a49e4984a94d17018fe58adafe18a1262755eeb
Lars Wiegers <larswiegers@live.nl>
2022-02-16 14:46:07 +0100

    Add in basic tests

---
+++ tests/TestCase.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use Gajus\Dindent\Indenter;
+use Larswiegers\LaravelMaps\LaravelMapsServiceProvider;
+
+class TestCase extends \Orchestra\Testbench\TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+        // additional setup
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            LaravelMapsServiceProvider::class,
+        ];
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        // perform environment setup
+    }
+
+    public function assertComponentRenders(string $expected, string $template, array $data = []): void
+    {
+        $indenter = new Indenter();
+        $indenter->setElementType('h1', Indenter::ELEMENT_TYPE_INLINE);
+        $indenter->setElementType('del', Indenter::ELEMENT_TYPE_INLINE);
+
+        $blade = (string) $this->blade($template, $data);
+        $indented = $indenter->indent($blade);
+        $cleaned = str_replace(
+            [' >', "\n/>", ' </div>', '> ', "\n>"],
+            ['>', ' />', "\n</div>", ">\n    ", '>'],
+            $indented,
+        );
+
+        $this->assertSame($expected, $cleaned);
+    }
+
+    public function getComponentRenderedContent(string $template, array $data = []): string
+    {
+        $indenter = new Indenter();
+        $indenter->setElementType('h1', Indenter::ELEMENT_TYPE_INLINE);
+        $indenter->setElementType('del', Indenter::ELEMENT_TYPE_INLINE);
+
+        $blade = (string) $this->blade($template, $data);
+        $indented = $indenter->indent($blade);
+        $cleaned = str_replace(
+            [' >', "\n/>", ' </div>', '> ', "\n>"],
+            ['>', ' />', "\n</div>", ">\n    ", '>'],
+            $indented,
+        );
+
+        return $cleaned;
+    }
+}
