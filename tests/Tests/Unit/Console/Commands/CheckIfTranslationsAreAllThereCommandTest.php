25c896fb8e49636f1b912c203d1e853e467b8a92
Lars Wiegers <larswiegers@live.nl>
2022-02-16 22:08:13 +0100

    add in the okay message

---
+++ tests/Tests/Unit/Console/Commands/CheckIfTranslationsAreAllThereCommandTest.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Console\Commands;
+
+use Tests\TestCase;
+
+final class CheckIfTranslationsAreAllThereCommandTest extends TestCase
+{
+
+    public $basicDir = "tests/resources/lang/basic/";
+    public $multipleLangs  = "tests/resources/lang/multi_langs/";
+
+    public function test_it_returns_errors_if_one_key_is_missing()
+    {
+        $command = $this->artisan('translations:check', [
+            '--directory' => $this->basicDir . 'one_missing_key'
+        ]);
+        $command->expectsOutput('Missing the translation with key: nl.test.test_key');
+    }
+
+    public function test_it_returns_errors_if_multiple_keys_are_missing()
+    {
+        $command = $this->artisan('translations:check', [
+            '--directory' =>  $this->basicDir . 'two_missing_keys'
+        ]);
+        
+        $command->expectsOutput('Missing the translation with key: nl.test.test_key');
+        $command->expectsOutput('Missing the translation with key: nl.test.test_key2');
+    }
+
+    public function test_it_fails_if_key_is_missing()
+    {
+        $command = $this->artisan('translations:check', [
+            '--directory' => $this->basicDir . 'one_missing_key'
+        ]);
+        $command->assertExitCode(1);
+    }
+
+    public function test_it_is_successful_if_none_keys_are_missing()
+    {
+        $command = $this->artisan('translations:check', [
+            '--directory' => $this->basicDir . 'zero_missing_keys'
+        ]);
+
+        $command->assertExitCode(0);
+    }
+
+    public function test_it_handles_a_single_language()
+    {
+        $command = $this->artisan('translations:check', [
+            '--directory' => $this->multipleLangs . 'one_language'
+        ]);
+
+        $command->assertExitCode(0);
+    }
+
+    public function test_it_handles_two_languages()
+    {
+        $command = $this->artisan('translations:check', [
+            '--directory' => $this->multipleLangs . 'two_languages'
+        ]);
+
+        $command->assertExitCode(0);
+    }
+
+    public function test_it_handles_ten_languages()
+    {
+        $command = $this->artisan('translations:check', [
+            '--directory' => $this->multipleLangs . 'ten_languages'
+        ]);
+
+        $command->assertExitCode(0);
+    }
+
+    public function test_it_returns_an_all_good_message_if_everything_is_good()
+    {
+        $command = $this->artisan('translations:check', [
+            '--directory' => $this->basicDir . 'zero_missing_keys'
+        ]);
+
+        $command->expectsOutput('✔ All translations are okay!');
+
+        $command->assertExitCode(0);
+    }
+}
